46a30dd848930cb774dc91e98f504642e5c81743
Vladislav Bakulin <vadis.bakulin@gmail.com>
2021-10-13 23:00:36 +0300

    style: исправить вёрстку

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,28 @@
 const modalWindow = document.querySelector('.popup');
 const modalWindowCloseBtn = modalWindow.querySelector('.popup__close');
 const profileEditBtn = document.querySelector('.profile__edit-button');
+const textName = document.querySelector('.profile__info .profile__name');
+const textVocation = document.querySelector('.profile__info .profile__vocation');
+const inputName = document.querySelector('.popup__text .popup__text-name');
+const inputVocation = document.querySelector('.popup__text .popup__text-vocation');
 
 function toggleModalWindow() {
     modalWindow.classList.toggle('popup_is-opened');
 }
 
-profileEditBtn.addEventListener('click', toggleModalWindow);
-modalWindowCloseBtn.addEventListener('click', toggleModalWindow);
 
-const textName = document.querySelector('.profile__info .profile__name');
-const textVocation = document.querySelector('.profile__info .profile__vocation');
 
-const inputName = document.querySelector('.popup__text .popup__text-name');
-const inputVocation = document.querySelector('.popup__text .popup__text-vocation');
+
 
 inputName.value = textName.textContent;
 inputVocation.value = textVocation.textContent;
 
 
 document.querySelector('.popup__save').onclick = function(){
-
+    event.preventDefault();
     textName.textContent = inputName.value;
     textVocation.textContent = inputVocation.value; 
 }
+
+profileEditBtn.addEventListener('click', toggleModalWindow);
+modalWindowCloseBtn.addEventListener('click', toggleModalWindow);
